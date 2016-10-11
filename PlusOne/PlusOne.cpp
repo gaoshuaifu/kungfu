5fbef74d3cb723590d3f7ed451c4efaf215258cf
wangyangtc <wangyangtc@gmail.com>
2016-10-10 22:21:56 -0700

    Change PlusOne

--- PlusOne/PlusOne.cpp
+++ PlusOne/PlusOne.cpp
@@ -10,45 +10,16 @@ using namespace std;
 class Solution{
 public:
   vector<int> plusOne(vector<int>& digits){
-    int n = digits.size();
-    int i;
-    for(i = n - 1; i >= 0; i--){
-      if(digits[i] == 9)
-        digits[i] = 0;
-      else{
-        digits[i]++;
-        break;
-      }
-    }
-
-    if(i < 0)
+    int i = digits.size() - 1;
+    while(digits[i] == 9)
+      digits[i--] = 0;
+    if(i >= 0)
+      digits[i] += 1;
+    else
       digits.insert(digits.begin(), 1);
-
     return digits;
   }
 };
-
-class Solution{
-public:
-  vector<int> plusOne(vector<int>& digits){
-    int n = digits.size();
-    int digit;
-    int carry = 1;
-    for(int i = n - 1; i >= 0; i--){
-      int sum = digits[i] + carry;
-      digit = sum % 10;
-      carry = sum / 10;
-      digits[i] = digit;
-      if(carry == 0)
-        break;
-    }
-    if(carry > 0)
-      digits.insert(digits.begin(), carry);
-
-    return digits;
-  }
-};
-
 
 int main(){
   int a[] = {9, 9};
@@ -61,4 +32,3 @@ int main(){
   cout << endl;
   return 0;
 }
-
