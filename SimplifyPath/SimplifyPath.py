4454f18bdb69ea9c9a9e3ab52e19769bd195b25d
wangyangtc <wangyangtc@gmail.com>
2016-10-24 21:01:07 -0700

    Add SimplifyPath.py

---
+++ SimplifyPath/SimplifyPath.py
@@ -0,0 +1,15 @@
+class Solution(object):
+  def simplifyPath(self, path):
+    tokens = path.split('/')
+    simple_tokens = []
+    for token in tokens:
+      if token == '..' and simple_tokens:
+        simple_tokens.pop()
+      elif token != '..' and token != '.' and token:
+        simple_tokens.append(token)
+    return '/' + '/'.join(simple_tokens)
+
+if __name__ == '__main__':
+    path = '/./home///ywang/../ywang/.profile/programming'
+    solution = Solution()
+    print(solution.simplifyPath(path))
