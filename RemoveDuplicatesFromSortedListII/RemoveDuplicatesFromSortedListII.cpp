91775c42d33767e150c65c6270086a8c91bdc665
wangyangtc <wangyangtc@gmail.com>
2016-10-26 20:07:05 -0700

    Change RemoveDuplicatesFromSortedListII

--- RemoveDuplicatesFromSortedListII/RemoveDuplicatesFromSortedListII.cpp
+++ RemoveDuplicatesFromSortedListII/RemoveDuplicatesFromSortedListII.cpp
@@ -47,18 +47,12 @@ public:
     ListNode* curr = head;
 
     while(curr){
-      ListNode* post = curr->next;
-      bool isDup = false;
-      while(post && curr->val == post->val){
-        curr->next = post->next;
-        delete post;
-        post = curr->next;
-        isDup = true;
-      }
-      if(isDup){
-        prev->next = curr->next;
-        delete curr;
-        curr = prev->next;
+      if(curr->next && curr->val == curr->next->val){
+        int val = curr->val;
+        while(curr && curr->val = val){
+          prev->next = curr->next;
+          delete curr;
+        }
       }
       else{
         prev = prev->next;
@@ -72,53 +66,6 @@ public:
   }
 };
 
-class Solution1{
-public:
-  ListNode *deleteDuplicates(ListNode *head) {
-    if(!head) return NULL;
-    ListNode* tmp = new ListNode(INT_MIN);
-    tmp->next = head;
-    ListNode* prev = tmp;
-    ListNode* curr = head;
-    ListNode* post = head->next;
-    bool isDup = false;
-
-    while(post){
-      if(curr->val == post->val){
-        curr->next = post->next;
-        delete post;
-        post = curr->next;
-        isDup = true;
-      }
-      else{
-        if(!isDup){
-          prev = prev->next;
-          curr = curr->next;
-          post = post->next;
-        }
-        else{
-          prev->next = curr->next;
-          delete curr;
-          curr = prev->next;
-          post = curr->next;
-          isDup = false;
-        }
-
-      }
-    }
-
-    if(isDup){
-      prev->next = NULL;
-      delete curr;
-    }
-
-    head = tmp->next;
-    delete tmp;
-    return head;
-  }
-};
-
-
 int main(){
   int array[] = {1, 1, 2, 3, 3, 4};
   ListNode* list = initializeList(array, sizeof(array) / sizeof(array[0]));
@@ -128,4 +75,3 @@ int main(){
   print(res);
   return 0;
 }
-
