6f55f2d7ddec6b97c7e7c204ca82364e0a96e1c7
wangyangtc <wangyangtc@gmail.com>
2016-10-28 14:16:16 -0700

    Change IntegerToRoman

--- IntegerToRoman/IntegerToRoman.cpp
+++ IntegerToRoman/IntegerToRoman.cpp
@@ -38,6 +38,24 @@ public:
   }
 };
 
+class Solution {
+public:
+  string intToRoman(int num) {
+    const vector<int> nums{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    const vector<string> romans{"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    string res;
+    int i = 0;
+    while (num > 0) {
+      while (num >= nums[i]) {
+        num -= nums[i];
+        res += romans[i];
+      }
+      i++;
+    }
+    return res;
+  }
+};
+
 int main(){
   int num = 3999;
 
@@ -46,4 +64,3 @@ int main(){
 
   return 0;
 }
-
