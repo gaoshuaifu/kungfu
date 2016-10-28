b6498bbc333a5611c1fb6e7c19eb9346a4d0086b
wangyangtc <wangyangtc@gmail.com>
2016-10-28 14:36:03 -0700

    Change RomanToInteger

--- RomanToInteger/RomanToInteger.cpp
+++ RomanToInteger/RomanToInteger.cpp
@@ -8,26 +8,22 @@ using namespace std;
 
 class Solution {
 public:
-  int mapping(char c){
-    switch(c){
-      case 'I': return 1;
-      case 'V': return 5;
-      case 'X': return 10;
-      case 'L': return 50;
-      case 'C': return 100;
-      case 'D': return 500;
-      case 'M': return 1000;
-    }
-  }
-
   int romanToInt(string s) {
+    unordered_map<char, int> mapping = {
+      {'I', 1}, 
+      {'V', 5},
+      {'X', 10},
+      {'L', 50},
+      {'C', 100},
+      {'D', 500},
+      {'M', 1000}};
     int n = s.size();
     int res = 0;
     for(int i = 0; i < n; i++){
-      if(mapping(s[i]) < mapping(s[i + 1]))
-        res -= mapping(s[i]);
+      if(mapping[s[i]] < mapping[s[i + 1]])
+        res -= mapping[s[i]];
       else
-        res += mapping(s[i]);
+        res += mapping[s[i]];
     }
     return res;
   }
