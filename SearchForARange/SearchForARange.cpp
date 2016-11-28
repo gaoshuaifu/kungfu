cd642eebeab7cbf661a6687aa8fa943b8413312c
wangyangtc <wangyangtc@gmail.com>
2016-11-27 20:59:10 -0800

    Change SearchForARange

--- SearchForARange/SearchForARange.cpp
+++ SearchForARange/SearchForARange.cpp
@@ -7,36 +7,38 @@ using namespace std;
 
 class Solution {
 public:
-  int searchLow(int A[], int n, int target){
+  int searchLow(vector<int>& nums, int target){
+    int n = nums.size();
     int low = 0;
     int high = n - 1;
     while(low <= high){
       int mid = low + (high - low) / 2;
-      if(target <= A[mid])
+      if(target <= nums[mid])
         high = mid - 1;
-      if(target > A[mid])
+      if(target > nums[mid])
         low = mid + 1;
     }
     return low;
   }
 
-  int searchHigh(int A[], int n, int target){
+  int searchHigh(vector<int>& nums, int target){
+    int n = nums.size();
     int low = 0;
     int high = n - 1;
     while(low <= high){
       int mid = low + (high - low) / 2;
-      if(target < A[mid])
+      if(target < nums[mid])
         high = mid - 1;
-      if(target >= A[mid])
+      if(target >= nums[mid])
         low = mid + 1;
     }
     return high;
   }
 
-  vector<int> searchRange(int A[], int n, int target) {
+  vector<int> searchRange(vector<int>& nums, int target) {
     vector<int> res(2, -1);
-    int low = searchLow(A, n, target);
-    int high = searchHigh(A, n, target);
+    int low = searchLow(nums, target);
+    int high = searchHigh(nums, target);
     if(low > high)
       return res;
     else{
@@ -57,4 +59,3 @@ int main(){
   cout << "[" << res[0] << ", " << res[1] << "]" << endl;
   return 0;
 }
-
