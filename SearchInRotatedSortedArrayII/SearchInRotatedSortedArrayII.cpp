20f18c90e79b30f9b83dc656f3731b836032e259
wangyangtc <wangyangtc@gmail.com>
2016-11-28 21:15:13 -0800

    Change SearchInRotatedSortedArrayII

--- SearchInRotatedSortedArrayII/SearchInRotatedSortedArrayII.cpp
+++ SearchInRotatedSortedArrayII/SearchInRotatedSortedArrayII.cpp
@@ -6,19 +6,40 @@ using namespace std;
 
 class Solution{
 public:
-  bool search(int A[], int n, int target){
-    int l = 0;
-    int h = n - 1;
-    for(int i = 0; i < n; i++)
-      if(A[i] == target)
+  int search(int A[], int n, int target){
+    int low = 0;
+    int high = n - 1;
+    while(low <= high){
+      while (low < high && A[low] == A[low + 1])
+        low++;
+      while (low < high && A[high] == A[high - 1])
+        high--;
+
+      int mid = low + (high - low) / 2;
+
+      if(target == A[mid])
         return true;
+
+      if(A[low] <= A[mid]){
+        if(A[low] <= target && target < A[mid])
+          high = mid - 1;
+        else
+          low = mid + 1;
+      }
+      else{
+        if(A[mid] < target && target <= A[high])
+          low = mid + 1;
+        else
+          high = mid - 1;
+      }
+    }
     return false;
   }
 };
 
 int main(){
-  int A[] = {4, 5, 6, 7, 0, 1, 2};
-  int target = 2;
+  int A[] = {1};
+  int target = 0;
 
   Solution solution;
   int res = solution.search(A, sizeof(A)/sizeof(A[0]), target);
@@ -26,6 +47,3 @@ int main(){
 
   return 0;
 }
-
-
-
