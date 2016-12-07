3675fb0cc6e3d8f782a0ec8545e7933ee21400f0
wangyangtc <wangyangtc@gmail.com>
2016-12-06 22:00:29 -0800

    Add DrawCircle

---
+++ DrawCircle/DrawCircle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  void drawCircle(x0, y0, r) {
+    for(int i = 0; i < 45; i++) {
+      double rx = r * cos(i);
+      double ry = r * sin(i);
+      plot(x + rx, y + ry);
+      plot(x + rx, y - ry);
+      plot(x - rx, y + ry);
+      plot(x - rx, y - ry);
+      plot(x + ry, y + rx);
+      plot(x + ry, y - rx);
+      plot(x - ry, y + rx);
+      plot(x - ry, y - rx);
+    }
+  }
+}
+
+int main() {
+  return 0;
+}
