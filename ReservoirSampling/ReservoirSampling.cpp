41e833cfc746dce38833db9410d5fc8a0d45b6d3
wangyangtc <wangyangtc@gmail.com>
2016-12-10 18:16:36 -0800

    Change ReservoirSampling

--- ReservoirSampling/ReservoirSampling.cpp
+++ ReservoirSampling/ReservoirSampling.cpp
@@ -4,37 +4,6 @@
 #include <ctime>
 using namespace std;
 
-struct ListNode{
-  int val;
-  ListNode* next;
-  ListNode(int val){
-    this->val = val;
-    this->next = NULL;
-  }
-};
-
-ListNode* initializeList(int array[], int n){
-  ListNode* head = NULL;
-  ListNode* p = NULL;
-
-  for(int i = 0; i < n; i++){
-    if(head == NULL){
-      head = new ListNode(array[i]);
-      p = head;
-    }
-    else{
-      p->next = new ListNode(array[i]);
-      p = p->next;
-    }
-  }
-  return head;
-}
-
-void print(ListNode* head){
-  for(ListNode* p = head; p != NULL; p = p->next)
-    cout << p->val << " ";
-  cout << endl;
-}
 
 // random number between 1 and n
 int rand1toN(int n){
@@ -48,24 +17,18 @@ int rand1toN(int n){
 
 class Solution{
 public:
-  vector<int> sample(ListNode* head, int k){
+  vector<int> sample(int n, int k){
     vector<int> res;
 
-    ListNode* curr = head;
-
-    int i;
-    for(i = 1; i <= k; i++){
-      res.push_back(curr->val);
-      curr = curr->next;
+    for(int i = 1; i <= k; i++){
+      res.push_back(i);
     }
 
     srand(time(NULL));
-    while(curr){
+    for(int i = k + 1; i <= n; i++){
       int r = rand1toN(i);
       if(r <= k)
-        res[r - 1] = curr->val;
-      curr = curr->next;
-      i++;
+        res[r - 1] = i;
     }
 
     return res;
@@ -73,14 +36,8 @@ public:
 };
 
 int main(){
-  int array[100];
-  for(int i = 1; i <= 100; i++)
-    array[i] = i;
-
-  ListNode* head = initializeList(array, sizeof(array) / sizeof(array[0]));
-
   Solution solution;
-  vector<int> res = solution.sample(head, 10);
+  vector<int> res = solution.sample(100, 10);
   for(int i = 0; i < res.size(); i++)
     cout << res[i] << " ";
   cout << endl;
