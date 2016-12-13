cc7d00bf091d86febb27281b4bd864c6fc2d718a
wangyangtc <wangyangtc@gmail.com>
2016-12-12 17:48:42 -0800

    Add MonotonicArray

---
+++ MonotonicArray/MonotonicArray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+  bool isMonotonic(vector<int>& nums) {
+    bool isUp = true;
+    bool isDown = true;
+
+    for(int i = 1; i < nums.size() && (isUp || isDown); i++) {
+      if(nums[i - 1] < nums[i])
+        isDown = false;
+      else if(nums[i - 1] > nums[i])
+        isUp = false;
+    }
+
+    return isUp || isDown;
+  }
+};
+
+int main() {
+  return 0;
+}
