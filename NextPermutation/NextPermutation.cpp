636404ee2d64610a15a0246745ef3bd4c5ce69eb
wangyangtc <wangyangtc@gmail.com>
2016-12-16 12:45:22 -0800

    Change NextPermutation

--- NextPermutation/NextPermutation.cpp
+++ NextPermutation/NextPermutation.cpp
@@ -31,6 +31,30 @@ public:
     swap(num[i], num[j]);
     reverse(num.begin() + i + 1, num.end());
   }
+
+  void prevPermutation(vector<int>& num){
+    int n = num.size();
+
+    int i;
+    for(i = n - 2; i >= 0; i--){
+      if(num[i] > num[i + 1])
+        break;
+    }
+
+    if(i < 0){
+      reverse(num.begin(), num.end());
+      return;
+    }
+
+    int j;
+    for(j = n - 1; j > i; j--){
+      if(num[j] < num[i])
+        break;
+    }
+
+    swap(num[i], num[j]);
+    reverse(num.begin() + i + 1, num.end());
+  }
 };
 
 
@@ -41,18 +65,24 @@ void print(vector<int>& num){
 }
 
 int main(){
-  int a[] = {1, 2, 3, 4};
-  vector<int> num = vector<int>(a, a + sizeof(a) / sizeof(a[0]));
-  print(num);
-
-
   Solution solution;
 
+  int a[] = {1, 2, 3, 4};
+  vector<int> num1 = vector<int>(a, a + sizeof(a) / sizeof(a[0]));
+  print(num1);
   for(int i = 0; i < 24; i++){
-    solution.nextPermutation(num);
-    print(num);
+    solution.nextPermutation(num1);
+    print(num1);
   }
 
+  cout << endl;
+
+  int b[] = {4, 3, 2, 1};
+  vector<int> num2 = vector<int>(b, b + sizeof(b) / sizeof(b[0]));
+  print(num2);
+  for(int i = 0; i < 24; i++){
+    solution.prevPermutation(num2);
+    print(num2);
+  }
   return 0;
 }
-
