cc3cfc0f409e9dbaa1fd031ca507a5e3b1298576
wangyangtc <wangyangtc@gmail.com>
2016-12-18 21:15:04 -0800

    Add SVNBisect

---
+++ SVNBisect/SVNBisect.cpp
@@ -0,0 +1,23 @@
+/*
+
+revision 123 <-- good
+revision 124
+...
+revision ??? <-- introduced the bug
+...
+revision 544
+revision 545 <-- bad
+
+*/
+
+int findBadRev(unsigned int good, unsigned int bad) {
+  while (good < bad - 1) {
+    unsigned int mid = good + (bad - good)/2;
+    if (isBad(mid)) {
+      bad = mid;
+    } else {
+      good = mid;
+    }
+  }
+  return bad;
+}
