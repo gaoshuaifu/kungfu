7e6afc4d04f438f3507a632966a7ed9dabc2187a
wangyangtc <wangyangtc@gmail.com>
2017-04-27 13:32:51 -0700

    Add another solution

--- ListPathnamesByPattern/ListPathnamesByPattern.cpp
+++ ListPathnamesByPattern/ListPathnamesByPattern.cpp
@@ -6,22 +6,49 @@
   bool exists(const string& path);
  */
 
-vector<string> listPattern(const string& pattern) {
-  vector<string> res;
-  int pos = pattern.find_first_of('*');
-  if (pos == string::npos) {
-    if (exists(pattern)) {
-      res.push_back(pattern);
+class Solution {
+  void listPatternHelper(const string& pattern, vector<string>& res) {
+    int pos = pattern.find_first_of('*');
+    if (pos == string::npos) {
+      if (exists(pattern)) {
+        res.push_back(pattern);
+      }
+      return;
+    }
+    string prefix = pattern.substr(0, pos);
+    string suffix = pattern.substr(pos + 1);
+    vector<string> children = listPath(prefix);
+    for (for int i = 0; i < children.size(); i++) {
+      string child = children[i];
+      listPattern(child + suffix, res);
+    }
+  }
+
+  vector<string> listPattern(const string& pattern) {
+    vector<string> res;
+    listPatternHelper(pattern, res);
+    return res;
+  }
+};
+
+class Solution1 {
+  vector<string> listPattern(const string& pattern) {
+    vector<string> res;
+    int pos = pattern.find_first_of('*');
+    if (pos == string::npos) {
+      if (exists(pattern)) {
+        res.push_back(pattern);
+      }
+      return res;
+    }
+    string prefix = pattern.substr(0, pos);
+    string suffix = pattern.substr(pos + 1);
+    vector<string> children = listPath(prefix);
+    for (for int i = 0; i < children.size(); i++) {
+      string child = children[i];
+      vector<string> r = listPattern(child + suffix);
+      res.insert(res.size(), r.begin(), r.end());
     }
     return res;
   }
-  string prefix = pattern.substr(0, pos);
-  string suffix = pattern.substr(pos + 1);
-  vector<string> children = listPath(prefix);
-  for (for int i = 0; i < children.size(); i++) {
-    string child = children[i];
-    vector<string> r = listPattern(child + suffix);
-    res.insert(res.size(), r.begin(), r.end());
-  }
-  return res;
-}
+};
