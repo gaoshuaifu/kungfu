e499dd5fcb472f52116f722c9d0ebd62b92c3d59
wangyangtc <wangyangtc@gmail.com>
2017-04-30 22:04:40 -0700

    add

---
+++ PancakeSort/PancakeSort.cpp
@@ -0,0 +1,16 @@
+
+// Sort a list using only flip(<list>) and largest_val(<list>) functions
+// where flip does a pancake flip
+void pancakeSort(vector<int>& nums) {
+  int n = nums.size();
+  for(int i = n - 1; i > 1; i--) {
+    // Find index of max value from a[0..i]
+    int maxIndex = findMax(nums, i);
+
+    // Move nums[maxIndex] to nums[i]
+    if(maxIndex != i) {
+      flip(nums, maxIndex);
+      flip(nums, i);
+    }
+  }
+}
