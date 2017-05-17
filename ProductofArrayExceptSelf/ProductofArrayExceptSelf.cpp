418e0f2bf751a69949045eecbf54d673c21869d1
wangyangtc <wangyangtc@gmail.com>
2017-05-17 14:23:25 -0700

    add another solution

--- ProductofArrayExceptSelf/ProductofArrayExceptSelf.cpp
+++ ProductofArrayExceptSelf/ProductofArrayExceptSelf.cpp
@@ -2,9 +2,33 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
+        vector<int> res(n, 1);
+
+        int left = 1;
+        for(int i = 1; i < n; i++) {
+            left *= nums[i - 1];
+            res[i] *= left;
+        }
+
+        int right = 1;
+        for(int i = n - 2; i >= 0; i--) {
+            right *= nums[i + 1];
+            res[i] *= right;
+        }
+
+        return res;
+    }
+};
+
+
+class Solution1 {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
         vector<int> left(n, 1);
-        left[0] = 1;
         vector<int> right(n, 1);
+
+        left[0] = 1;
         left[n - 1] = 1;
         for(int i = 1; i < n; i++) {
             left[i] = left[i - 1] * nums[i - 1];
