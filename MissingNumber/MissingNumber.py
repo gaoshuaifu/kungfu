b6bfa9f08a96a5f50a4231fea2a4f9c777e2d96c
wangyangtc <wangyangtc@gmail.com>
2017-05-17 17:01:12 -0700

    add another solution

--- MissingNumber/MissingNumber.py
+++ MissingNumber/MissingNumber.py
@@ -2,3 +2,16 @@ class Solution(object):
     def missingNumber(self, nums):
         n = len(nums)
         return n * (n + 1) / 2 - sum(nums)
+
+# If nums is sorted, should use binary search
+class Solution(object):
+    def missingNumber(self, nums):
+        nums = sorted(nums)
+        low, high = 0, len(nums)
+        while low < high:
+            mid = (low + high) / 2
+            if nums[mid] == mid:
+                low = mid + 1
+            else:  # if nums[mid] > mid
+                high = mid
+        return low
