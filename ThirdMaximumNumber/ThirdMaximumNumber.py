d222817959ab4df0870421d0cefcad35c58edc47
wangyangtc <wangyangtc@gmail.com>
2017-05-18 17:37:28 -0700

    change

--- ThirdMaximumNumber/ThirdMaximumNumber.py
+++ ThirdMaximumNumber/ThirdMaximumNumber.py
@@ -1,26 +1,8 @@
 class Solution(object):
     def thirdMax(self, nums):
-        n = len(nums)
-        i = 0
-        heap = []
-        while i < n and len(heap) < 3:
-            if nums[i] not in heap:
-                heap.append(nums[i])
-            i += 1
-
-        if len(heap) == 1:
-            return heap[0]
-        if len(heap) == 2:
-            return max(heap)
-
-        heap.sort(reverse=True)
-
-        while i < n:
-            if nums[i] not in heap:
-                if nums[i] > heap[-1]:
-                    heap.pop()
-                    heap.append(nums[i])
-                    heap.sort(reverse=True)
-            i += 1
-
-        return heap[-1]
+        s = set()
+        for num in nums:
+            s.add(num)
+            if len(s) > 3:
+                s.discard(min(s))
+        return min(s) if len(s) == 3 else max(s)
