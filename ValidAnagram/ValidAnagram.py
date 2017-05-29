70ae2d1a5cc2f004b179e449982905e1c064e0fe
wangyangtc <wangyangtc@gmail.com>
2017-05-29 11:06:58 -0700

    simplify

--- ValidAnagram/ValidAnagram.py
+++ ValidAnagram/ValidAnagram.py
@@ -1,16 +1,5 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        import collections
-
-        d = collections.defaultdict(int)
-        for c in s:
-            d[c] += 1
-
-        for c in t:
-            if c not in d:
-                return False
-            d[c] -= 1
-            if d[c] == 0:
-                del d[c]
-
-        return not d
+        from collections import Counter
+        sc, tc = Counter(s), Counter(t)
+        return sc == tc
