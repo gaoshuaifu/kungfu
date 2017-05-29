f5ea5359216677c37f44e7663e849d9d9cb55318
wangyangtc <wangyangtc@gmail.com>
2017-05-29 13:22:25 -0700

    add comment

--- ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.py
+++ ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.py
@@ -1,3 +1,7 @@
+# Scan the array from left to right and keep track of max value so far.
+# If the current number is not the max value, it should be included in the subarray to sort.
+# Scan the array from right to left and keep track of min value so far.
+# If the current number is not the min value, it should be included in the subarray to sort.
 class Solution(object):
     def findUnsortedSubarray(self, nums):
         n = len(nums)
@@ -11,34 +15,5 @@ class Solution(object):
             min_val = min(min_val, nums[n - 1 - i])
             if nums[n - 1 - i] > min_val:
                 start = n - 1 - i
-
-        return 0 if start is None else end - start + 1
-
-class Solution1(object):
-    def findUnsortedSubarray(self, nums):
-        import sys
-        n = len(nums)
-
-        min_val = sys.maxint
-        for i in range(1, n):
-            if nums[i - 1] > nums[i] and min_val > nums[i]:
-                min_val = nums[i]
-
-        start = None
-        for i in range(n):
-            if nums[i] > min_val:
-                start = i
-                break
-
-        max_val = -sys.maxint
-        for i in range(n - 2, -1, -1):
-            if nums[i] > nums[i + 1] and max_val < nums[i]:
-                max_val = nums[i]
-
-        end = None
-        for i in range(n - 1, -1, -1):
-            if nums[i] < max_val:
-                end = i
-                break
 
         return 0 if start is None else end - start + 1
