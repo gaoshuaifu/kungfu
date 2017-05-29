557232f443dddb1d70c4b18122d33386c7124ea6
wangyangtc <wangyangtc@gmail.com>
2017-05-29 11:21:51 -0700

    add comment

--- HappyNumber/HappyNumber.py
+++ HappyNumber/HappyNumber.py
@@ -1,4 +1,5 @@
 class Solution(object):
+    # Pass an integer to a list of digits.
     def _parse(self, n):
         digits = []
         while n > 0:
@@ -9,9 +10,15 @@ class Solution(object):
     def isHappy(self, n):
         seen = set()
         while True:
+            # Computer sum of squares of digits.
             n = sum(map(lambda x: x * x, self._parse(n)))
+
+            # If it is 1, then happy.
             if n == 1:
                 return True
+
+            # If it is seen before, then not happy.
             if n in seen:
                 return False
+
             seen.add(n)
