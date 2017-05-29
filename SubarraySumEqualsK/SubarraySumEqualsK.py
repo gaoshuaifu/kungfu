eeda45cd4eacdf3d9d8bf34838327d08ececf76f
wangyangtc <wangyangtc@gmail.com>
2017-05-29 14:41:32 -0700

    add comment

--- SubarraySumEqualsK/SubarraySumEqualsK.py
+++ SubarraySumEqualsK/SubarraySumEqualsK.py
@@ -1,14 +1,14 @@
 class Solution(object):
     def subarraySum(self, nums, k):
         from collections import defaultdict
-
+        # The dict (value => the number of subarrays nums[0:i] whose sum equals to value)
         preSumCount = defaultdict(int)
         preSumCount[0] = 1
         res = 0
-        sum = 0
+        s = 0
         for num in nums:
-            sum += num
-            if(sum - k in preSumCount):
-                res += preSumCount[sum - k]
-            preSumCount[sum] += 1
+            s += num
+            if(s - k in preSumCount):
+                res += preSumCount[s - k]
+            preSumCount[s] += 1
         return res
