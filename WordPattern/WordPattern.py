4fbdaf4252ea618bf26f2e4ae0ff95534261487d
wangyangtc <wangyangtc@gmail.com>
2017-05-29 10:34:39 -0700

    change

--- WordPattern/WordPattern.py
+++ WordPattern/WordPattern.py
@@ -4,14 +4,17 @@ class Solution(object):
         if len(p) != len(words):
             return False
 
+        # char and word have to have 1-to-1 relationship
         d1, d2 = {}, {}
         for c, w in zip(p, words):
+            # when chars are the same, the corresponding words should be the same
             if c not in d1:
                 d1[c] = w
             else:
                 if w != d1[c]:
                     return False
 
+            # when words are the same, the corresponding chars should be the same
             if w not in d2:
                 d2[w] = c
             else:
