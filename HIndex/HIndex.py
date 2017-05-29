d77a7bdd4426fb90616ca8477cce04d858b3b018
wangyangtc <wangyangtc@gmail.com>
2017-05-29 10:58:33 -0700

    add comment

--- HIndex/HIndex.py
+++ HIndex/HIndex.py
@@ -1,16 +1,21 @@
 class Solution(object):
     def hIndex(self, citations):
         n = len(citations)
+
+        # Create a new vector of size n+1.
+        # The h-index can be at most n, when all of his papers have citations >= n.
         count = [0] * (n + 1)
         for citation in citations:
+            # Put all citation > n in the same bucket.
             if citation > n:
                 count[n] += 1
             else:
                 count[citation] += 1
 
-        h = 0
+        # Scan citation from right (n) to left (0).
+        papers = 0
         for citation in range(n, -1, -1):
-            h += count[citation]
-            if h >= citation:
+            papers += count[citation]
+            if papers >= citation:
                 return citation
         return 0
