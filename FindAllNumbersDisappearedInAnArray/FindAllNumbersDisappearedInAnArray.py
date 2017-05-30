3a15141ab39be85838816ea3aa627c8d7af8cd39
wangyangtc <wangyangtc@gmail.com>
2017-05-29 19:31:25 -0700

    add comment

--- FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray.py
+++ FindAllNumbersDisappearedInAnArray/FindAllNumbersDisappearedInAnArray.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def findDisappearedNumbers(self, nums):
+        # 1 ≤ nums[i] ≤ n, so put nums[i] into index nums[i] - 1
         i = 0
         while i < len(nums):
             j = nums[i] - 1
@@ -8,6 +9,7 @@ class Solution(object):
             else:
                 i += 1
 
+        # Scan the array, if the value at index i is not i + 1, then i + 1 is a number disappeared
         res = []
         for i in range(len(nums)):
             if nums[i] != i + 1:
