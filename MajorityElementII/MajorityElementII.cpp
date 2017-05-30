ed46daecd9604e515e339abe1997f1abb8ebca3e
wangyangtc <wangyangtc@gmail.com>
2017-05-29 21:27:46 -0700

    add comment

--- MajorityElementII/MajorityElementII.cpp
+++ MajorityElementII/MajorityElementII.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        // Get the top two numbers with highest frequency
         int count1 = 0;
         int count2 = 0;
         int num1;
         int num2;
-
         for(auto num: nums){
             if (num1 == num){
                 count1++;
@@ -27,6 +27,7 @@ public:
             }
         }
 
+        // Count the frequency of those two numbers
         count1 = 0;
         count2 = 0;
         for(auto num: nums){
@@ -38,10 +39,11 @@ public:
             }
         }
 
+        // Check if their frequency > n/3
         vector<int> res;
-        if (count1 > nums.size()/3)
+        if (count1 > nums.size() / 3)
           res.push_back(num1);
-        if (count2 > nums.size()/3)
+        if (count2 > nums.size() / 3)
           res.push_back(num2);
 
         return res;
