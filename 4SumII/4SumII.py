2102f0413dd7ed56d7dc4a0584a1974e4b65ac39
wangyangtc <wangyangtc@gmail.com>
2017-06-04 16:27:28 -0700

    init

---
+++ 4SumII/4SumII.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def fourSumCount(self, A, B, C, D):
+        from collections import Counter
+        AB = Counter([a + b for a in A for b in B])
+        CD = Counter([c + d for c in C for d in D])
+        res = 0
+        for k in AB.keys():
+            if -k in CD:
+                res += AB[k] * CD[-k]
+        return res
