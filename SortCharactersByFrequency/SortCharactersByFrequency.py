75c882a907e72f477cc692606cefe43f0bbcc802
wangyangtc <wangyangtc@gmail.com>
2017-06-04 17:42:59 -0700

    init

---
+++ SortCharactersByFrequency/SortCharactersByFrequency.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def frequencySort(self, s):
+        from collections import Counter
+        c = Counter(list(s))
+        res = ""
+        for k, v in sorted(c.items(), key=lambda x : x[1], reverse=True):
+            res += k * v
+        return "".join(res)
