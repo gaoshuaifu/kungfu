a0156c9565c7690e201d7f5be94a7adcb5a029e2
wangyangtc <wangyangtc@gmail.com>
2017-06-08 12:08:21 -0700

    change

--- Pow/Pow.cpp
+++ Pow/Pow.cpp
@@ -5,40 +5,28 @@
 #include <iostream>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    double pow(double x, int n){
-        if(n == 0)
+    double myPow(double x, int n) {
+        if (n == 0) {
             return 1;
+        }
 
-        if(x == 0)
-            return 0;
-
-        if(x == 1)
-            return 1;
-
-        if(x == -1)
-            return n % 2? -1 : 1;
-
-        if(n < 0)
-            return 1.0 / pow(x, -n);
-
-        double half = pow(x, n / 2);
-        if(n % 2)
-            return x * half * half;
-        else
-            return half * half;
+        // -2147483648 == 2147483648 is true
+        if (n < 0 && n != -n) {
+            return 1.0 / myPow(x, -n);
+        }
+        double v = myPow(x, n / 2);
+        return n % 2? v * v * x : v * v;
     }
 };
 
-
 int main(){
-    double x = 0;
-    int n = 8;
+    double x = 1.0;
+    int n = -2147483648;
 
     Solution solution;
-    double res = solution.pow(x, n);
+    double res = solution.myPow(x, n);
     cout << res << endl;
     return 0;
 }
-
