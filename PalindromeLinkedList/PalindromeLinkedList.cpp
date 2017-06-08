c2254b89afdfae05ad937f5e7af3e5f84c516483
wangyangtc <wangyangtc@gmail.com>
2017-06-08 10:56:28 -0700

    change

--- PalindromeLinkedList/PalindromeLinkedList.cpp
+++ PalindromeLinkedList/PalindromeLinkedList.cpp
@@ -1,15 +1,18 @@
+
+// Reverse the 2nd half of the list.
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        if(!head || !head->next)
-            return true;
         ListNode* slow = head;
         ListNode* fast = head;
-        while(fast->next && fast->next->next) {
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
         }
-        slow = slow->next;
+        if(fast) {
+            slow = slow->next;
+        }
+
         slow = reverseList(slow);
         while(slow) {
             if(head->val != slow->val)
@@ -29,5 +32,30 @@ public:
             head = post;
         }
         return prev;
+    }
+};
+
+// Put the 1st half of the list into a stk.
+class Solution1 {
+public:
+    bool isPalindrome(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        stack<ListNode*> stk;
+        while(fast && fast->next){
+            stk.push(slow);
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        if(fast)
+            slow = slow->next;
+
+        while(slow){
+            if(slow->val != stk.top()->val)
+                return false;
+            slow = slow->next;
+            stk.pop();
+        }
+        return true;
     }
 };
