b1e60c2640d139221bdaa7761034dfc54c655f1a
wangyangtc <wangyangtc@gmail.com>
2017-06-08 18:37:27 -0700

    change

--- RectangleArea/RectangleArea.py
+++ RectangleArea/RectangleArea.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def rectArea(self, x1, y1, x2, y2):
-        return (x2 - x1) * (y2 - y1) if x1 < x2 or y1 < y2 else 0
+        return (x2 - x1) * (y2 - y1) if x1 < x2 and y1 < y2 else 0
 
     def computeArea(self, A, B, C, D, E, F, G, H):
         area1 = self.rectArea(A, B, C, D)
