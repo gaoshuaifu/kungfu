490e7860dd771f86886809ea872b5c01eefb7031
wangyangtc <wangyangtc@gmail.com>
2017-06-10 16:15:28 -0700

    add comment

--- BulbSwitcher/BulbSwitcher.py
+++ BulbSwitcher/BulbSwitcher.py
@@ -1,3 +1,5 @@
+# A bulb ends up on iff it has an odd number of divisors (switched an odd number of times).
+# Only square number has odd number of divisors.
 class Solution(object):
     def bulbSwitch(self, n):
         return int(n ** 0.5)
