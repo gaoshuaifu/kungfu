7c1d13546793766714de9e7f0ed8deeee53e8e9b
wangyangtc <wangyangtc@gmail.com>
2017-06-18 22:12:35 -0700

    rename

---
+++ LinkedListCycleII/LinkedListCycleII.cpp
@@ -0,0 +1,26 @@
+class Solution{
+public:
+    ListNode* loopBeginning(ListNode* head){
+        ListNode* slow = head;
+        ListNode* fast = head;
+        ListNode* meet = NULL;
+        while(slow && fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow == fast){
+                meet = slow;
+                break;
+            }
+        }
+
+        if(!meet) return NULL;
+
+        slow = head;
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
