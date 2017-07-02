37e3394e98e11928ce2f5d02553351e6fe3146fa
wangyangtc <wangyangtc@gmail.com>
2017-07-01 17:52:24 -0700

    add another solution

--- FractionAdditionAndSubtraction/FractionAdditionAndSubtraction.py
+++ FractionAdditionAndSubtraction/FractionAdditionAndSubtraction.py
@@ -7,3 +7,17 @@ class Solution(object):
         )
         res = sum(fracs)
         return "{}/{}".format(res.numerator, res.denominator)
+
+class Solution(object):
+    def fractionAddition(self, expression):
+        import fractions
+        ints = map(int, re.findall('[+-]?\d+', expression))
+        A, B, i = 0, 1, 0
+        while i < len(ints):
+            a, b = ints[i], ints[i + 1]
+            A, B = A * b + a * B, B * b
+            g = fractions.gcd(A, B)
+            A //= g
+            B //= g
+            i += 2
+        return "{}/{}".format(A, B)
