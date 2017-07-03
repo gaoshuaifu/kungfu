77b12c86a4dbf59a9fce48de3e9093ea5a50c707
wangyangtc <wangyangtc@gmail.com>
2017-07-03 11:49:50 -0700

    init

--- AddStrings/AddStrings.py
+++ AddStrings/AddStrings.py
@@ -5,13 +5,15 @@ class Solution(object):
         while i >= 0 or j >= 0:
             s = 0
             if i >= 0:
-                s += num1[i]
+                s += int(num1[i])
+                i -= 1
             if j >= 0:
-                s += num2[j]
+                s += int(num2[j])
+                j -= 1
             s += carry
             digit = s % 10
             carry = s / 10
             res.append(digit)
         if carry:
             res.append(carry)
-        return "".join(map(str, ))
+        return "".join(map(str, res[::-1]))
