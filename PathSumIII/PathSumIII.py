5489a5ba96a79afeac0185e8bb46993a830a09d1
wangyangtc <wangyangtc@gmail.com>
2017-07-04 22:47:55 -0700

    init

---
+++ PathSumIII/PathSumIII.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def pathSumHelper(self, root, target, sum_so_far, pre_sum):
+        if not root:
+            return 0
+
+        sum_so_far += root.val
+        if (sum_so_far - target) in pre_sum:
+            self.res += pre_sum[sum_so_far - target]
+
+        pre_sum[sum_so_far] += 1
+        self.pathSumHelper(root.left, target, sum_so_far, pre_sum)
+        self.pathSumHelper(root.right, target, sum_so_far, pre_sum)
+        pre_sum[sum_so_far] -= 1
+
+
+    def pathSum(self, root, target):
+        from collections import defaultdict
+        pre_sum = defaultdict(int)
+        pre_sum[0] = 1
+
+        self.res = 0
+        self.pathSumHelper(root, target, 0, pre_sum)
+        return self.res
