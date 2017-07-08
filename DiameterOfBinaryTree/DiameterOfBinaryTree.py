af101a3e50a805e4446d9b5729107b69342ef58f
wangyangtc <wangyangtc@gmail.com>
2017-07-08 12:01:03 -0700

    init

--- DiameterOfBinaryTree/DiameterOfBinaryTree.py
+++ DiameterOfBinaryTree/DiameterOfBinaryTree.py
@@ -2,15 +2,12 @@ class Solution(object):
     def diameterOfBinaryTreeHelper(self, node):
         if not node:
             return 0, 0
-
         left_diameter, left_hight = self.diameterOfBinaryTreeHelper(node.left)
         right_diameter, right_hight = self.diameterOfBinaryTreeHelper(node.right)
-
-        return (
-            max(left_diameter, right_diameter, left_hight + right_hight),
-            max(left_hight, right_hight) + 1
-        )
+        diameter = max(left_diameter, right_diameter, left_hight + right_hight)
+        height = max(left_hight, right_hight) + 1
+        return diameter, height
 
     def diameterOfBinaryTree(self, root):
-        diameter, height = self.diameterOfBinaryTreeHelper(root)
+        diameter, _ = self.diameterOfBinaryTreeHelper(root)
         return diameter
