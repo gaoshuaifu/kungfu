506d7a563603999e2c9df38940cc2f13f885ef71
wangyangtc <wangyangtc@gmail.com>
2017-07-09 13:35:36 -0700

    init

---
+++ GasStation/GasStation.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def canCompleteCircuit(self, gas, cost):
+        start, tank, total = 0, 0, 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                total += tank
+                tank = 0
+                start = i + 1
+        total += tank
+        return start if total > 0 else -1
