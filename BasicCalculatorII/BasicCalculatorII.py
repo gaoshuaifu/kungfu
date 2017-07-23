04ed82ac7cd8f9c1c2537bfb4139d57feed65ac0
wangyangtc <wangyangtc@gmail.com>
2017-07-22 18:22:05 -0700

    init

--- BasicCalculatorII/BasicCalculatorII.py
+++ BasicCalculatorII/BasicCalculatorII.py
@@ -18,3 +18,25 @@ class Solution(object):
                 curr_term = int(token)
         total += sign * curr_term
         return total
+
+
+class Solution(object):
+    def calculate(self, s):
+        s = s.replace(' ', '')
+        n, stk, sign, num = len(s), [], '+', 0
+        for i in xrange(n):
+            if s[i].isdigit():
+                num = 10 * num + ord(s[i]) - ord('0')
+            if not s[i].isdigit() or i == n - 1:
+                if sign == '+':
+                    stk.append(num)
+                elif sign == '-':
+                    stk.append(-num)
+                elif sign == '*':
+                    stk[-1] = stk[-1] * num
+                else:
+                    tmp = stk[-1] / num
+                    stk[-1] = tmp if tmp > 0 else -(abs(stk[-1]) / abs(num))
+                sign = s[i]
+                num = 0
+        return sum(stk)
