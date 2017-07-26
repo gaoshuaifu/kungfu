b81cdf771564da8dd1467124722bdcac1dd80061
wangyangtc <wangyangtc@gmail.com>
2017-07-25 21:19:38 -0700

    init

---
+++ SmallestRange/SmallestRange.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def smallestRange(self, nums):
+        import sys
+        indices = [0] * len(nums)
+        res = [-sys.maxint, sys.maxint]
+        while True:
+            values = [array[i] for array, i in zip(nums, indices)]
+            left, right = min(values), max(values)
+            if right - left < res[1] - res[0]:
+                res = [left, right]
+            min_index = values.index(left)
+            indices[min_index] += 1
+            if indices[min_index] == len(nums[min_index]):
+                break
+        return res
+
+class Solution(object):
+    def smallestRange(self, nums):
+        import sys, heapq
+        heap = [(num[0], i, 0) for i, num in enumerate(nums)]
+        heapq.heapify(heap)
+        right = max(heap)[0]
+        res = [-sys.maxint, sys.maxint]
+        while True:
+            left, i, j = heapq.heappop(heap)
+            if right - left < res[1] - res[0]:
+                res = [left, right]
+            if j + 1 == len(nums[i]):
+                break
+            v = nums[i][j+1]
+            heapq.heappush(heap, (v, i, j + 1))
+            right = max(right, v)
+        return res
