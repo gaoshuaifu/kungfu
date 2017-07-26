cc3c0c2c3038f5f4fac3c7e2643c75d936fc22c1
wangyangtc <wangyangtc@gmail.com>
2017-07-25 21:20:47 -0700

    init

--- SmallestRange/SmallestRange.py
+++ SmallestRange/SmallestRange.py
@@ -1,21 +1,5 @@
 class Solution(object):
     def smallestRange(self, nums):
-        import sys
-        indices = [0] * len(nums)
-        res = [-sys.maxint, sys.maxint]
-        while True:
-            values = [array[i] for array, i in zip(nums, indices)]
-            left, right = min(values), max(values)
-            if right - left < res[1] - res[0]:
-                res = [left, right]
-            min_index = values.index(left)
-            indices[min_index] += 1
-            if indices[min_index] == len(nums[min_index]):
-                break
-        return res
-
-class Solution(object):
-    def smallestRange(self, nums):
         import sys, heapq
         heap = [(num[0], i, 0) for i, num in enumerate(nums)]
         heapq.heapify(heap)
@@ -30,4 +14,20 @@ class Solution(object):
             v = nums[i][j+1]
             heapq.heappush(heap, (v, i, j + 1))
             right = max(right, v)
+        return res
+
+class Solution(object):
+    def smallestRange(self, nums):
+        import sys
+        indices = [0] * len(nums)
+        res = [-sys.maxint, sys.maxint]
+        while True:
+            values = [array[i] for array, i in zip(nums, indices)]
+            left, right = min(values), max(values)
+            if right - left < res[1] - res[0]:
+                res = [left, right]
+            min_index = values.index(left)
+            indices[min_index] += 1
+            if indices[min_index] == len(nums[min_index]):
+                break
         return res
