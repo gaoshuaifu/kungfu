7a54aaa9eff79cc17f812514770dac630eefbe46
wangyangtc <wangyangtc@gmail.com>
2017-07-30 23:26:04 -0700

    init

--- LongestIncreasingPathInAMatrix/LongestIncreasingPathInAMatrix.py
+++ LongestIncreasingPathInAMatrix/LongestIncreasingPathInAMatrix.py
@@ -1,5 +1,30 @@
 # DFS and cache/dp work together.
 class Solution(object):
+    DIRS = [(1,0),(-1,0),(0,1),(0,-1)]
+
+    def in_bound(self, matrix, i, j):
+        return i >= 0 and i < len(matrix) and j >= 0 and j < len(matrix[0])
+
+    def dfs(self, matrix, i, j, cache):
+        if cache[i][j] == 0:
+            max_length = 1
+            for di, dj in Solution.DIRS:
+                x, y = i + di, j + dj
+                if self.in_bound(matrix, x, y) and matrix[x][y] > matrix[i][j]:
+                    length = 1 + self.dfs(matrix, x, y, cache)
+                    max_length = max(max_length, length)
+            cache[i][j] = max_length
+        return cache[i][j]
+
+    def longestIncreasingPath(self, matrix):
+        if not matrix or not matrix[0]:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        cache = [[0] * n for _ in range(m)]
+        return max([self.dfs(matrix, i, j, cache) for i in range(m) for j in range(n)])
+
+
+class Solution(object):
     def longestIncreasingPath(self, matrix):
         def dfs(i, j):
             if cache[i][j] == 0:
