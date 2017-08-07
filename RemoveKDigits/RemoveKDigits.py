584fd98c84bb2847fdc84137739a3e1747f236c2
wangyangtc <wangyangtc@gmail.com>
2017-08-06 23:30:17 -0700

    change

--- RemoveKDigits/RemoveKDigits.py
+++ RemoveKDigits/RemoveKDigits.py
@@ -1,19 +1,19 @@
 class Solution(object):
     def removeKdigits(self, num, k):
         res = ""
-        while k:
-            if k == len(num):
-                return "0" if res == "" else res
-
+        while k and k < len(num):
             min_dgt = min(num[:k + 1])
             res += min_dgt
             min_idx = num.find(min_dgt)
             num = num[min_idx + 1:]
             k -= min_idx
-        res += num
 
-        i = 0
-        while i < len(res) and res[i] == "0":
-            i += 1
+        if k == len(num):
+            return res if res else "0"
 
-        return "0" if i == len(res) else res[i:]
+        res = list(res + num)
+
+        while res and res[0] == "0":
+            res.pop(0)
+
+        return "".join(res) if res else "0"
