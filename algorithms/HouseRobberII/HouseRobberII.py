921bd0e183c24c83b78fdd9d766c68a7ccbbb834
wangyangtc <wangyangtc@gmail.com>
2017-09-27 13:21:34 -0700

    add

---
+++ algorithms/HouseRobberII/HouseRobberII.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def rob_helper(self, nums):
+        n = len(nums)
+        if n == 0: return 0
+        if n == 1: return nums[0]
+        dp = [0] * 3
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i % 3] = max(dp[(i - 2) % 3] + nums[i], dp[(i - 1) % 3]);
+        return dp[(n - 1) % 3]
+
+    def rob(self, nums):
+        n = len(nums)
+        if n == 1: return nums[0]
+        return max(self.rob_helper(nums[1:]), self.rob_helper(nums[:-1]))
