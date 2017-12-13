a08c9a59834b0857b3856be0f3ac6bfd4910b815
wangyangtc <wangyangtc@gmail.com>
2017-12-13 15:25:29 -0800

    add another solution to ZigzagIterator

--- algorithms/ZigzagIterator/ZigzagIterator.py
+++ algorithms/ZigzagIterator/ZigzagIterator.py
@@ -1,4 +1,32 @@
 class ZigzagIterator(object):
+    def __init__(self, v1, v2):
+        """
+        Initialize your data structure here.
+        :type v1: List[int]
+        :type v2: List[int]
+        """
+
+        self.queue = [v for v in [v1, v2] if v]
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        v = self.queue.pop(0)
+        res = v.pop(0)
+        if v:
+            self.queue.append(v)
+        return res
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+
+        return self.queue
+
+
+class ZigzagIterator(object):
 
     def __init__(self, v1, v2):
         """
