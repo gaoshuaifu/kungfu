025647bc5a4932bf9ef7640387581f82c1f3f2eb
wangyangtc <wangyangtc@gmail.com>
2017-12-16 21:46:55 -0800

    add another solution

--- algorithms/BestTimeToBuyAndSellStockII/BestTimeToBuyAndSellStockII.py
+++ algorithms/BestTimeToBuyAndSellStockII/BestTimeToBuyAndSellStockII.py
@@ -6,3 +6,13 @@ class Solution(object):
             if prices[i] > prices[i - 1]:
                 profit += prices[i] - prices[i - 1]
         return profit
+
+class Solution(object):
+    def maxProfit(self, prices):
+        from sys import maxsize
+        buy = -maxsize
+        sell = 0
+        for price in prices:
+            buy = max(buy, sell - price)
+            sell = max(sell, buy + price)
+        return sell
