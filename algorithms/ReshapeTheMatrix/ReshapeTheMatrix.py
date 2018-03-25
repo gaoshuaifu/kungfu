f861d1ef0e64bbc43ea6e7a68cfdcc73f4490741
wangyangtc <wangyangtc@gmail.com>
2018-03-25 13:10:01 -0700

    minor

--- algorithms/ReshapeTheMatrix/ReshapeTheMatrix.py
+++ algorithms/ReshapeTheMatrix/ReshapeTheMatrix.py
@@ -1,12 +1,8 @@
 class Solution(object):
     def matrixReshape(self, nums, r, c):
-        if (len(nums) == r and len(nums[0]) == c) or (len(nums) * len(nums[0]) != r * c):
+        if len(nums) * len(nums[0]) != r * c:
             return nums
-        if c == 0:
-            return [] * r
-
-        res = []
-        sub = []
+        res, sub = [], []
         for row in nums:
             for num in row:
                 sub.append(num)
