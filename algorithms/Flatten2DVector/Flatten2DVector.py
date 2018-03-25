54589d94f8c3c328b009f1392b37d54a4f753781
wangyangtc <wangyangtc@gmail.com>
2018-03-25 12:53:44 -0700

    minor

--- algorithms/Flatten2DVector/Flatten2DVector.py
+++ algorithms/Flatten2DVector/Flatten2DVector.py
@@ -3,26 +3,26 @@ class Vector2D(object):
         """ Initialize your data structure here. vec2d: List[List[int]] """
 
         self.vec2d = vec2d
-        self.x = 0
+        self.i = 0
         self._find_next_list()
 
     def _find_next_list(self):
-        while self.x < len(self.vec2d) and not self.vec2d[self.x]:
-            self.x += 1
-        self.y = 0
+        while self.i < len(self.vec2d) and not self.vec2d[self.i]:
+                self.i += 1
+        self.j = 0
 
     def next(self):
         """ rtype: int """
 
-        res = self.vec2d[self.x][self.y]
+        res = self.vec2d[self.i][self.j]
 
-        self.y += 1
-        if self.y == len(self.vec2d[self.x]):
-            self.x += 1
+        self.j += 1
+        if self.j == len(self.vec2d[self.i]):
+            self.i += 1
             self._find_next_list()
 
         return res
 
     def hasNext(self):
         """ :rtype: bool """
-        return self.x < len(self.vec2d) and self.y < len(self.vec2d[self.x])
+        return self.i < len(self.vec2d) and self.j < len(self.vec2d[self.i])
