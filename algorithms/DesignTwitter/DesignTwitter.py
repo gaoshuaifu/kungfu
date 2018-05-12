5470b6a8e9a2d836dc25243d7e9a1cf4d98f2c07
wangyangtc <wangyangtc@gmail.com>
2018-05-11 22:39:56 -0700

    minor

--- algorithms/DesignTwitter/DesignTwitter.py
+++ algorithms/DesignTwitter/DesignTwitter.py
@@ -24,7 +24,10 @@ class Twitter(object):
 
     def getNewsFeed(self, userId):
         """
-        Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent.
+        Retrieve the 10 most recent tweet ids in the user's news feed.
+        Each item in the news feed must be posted by users who the user
+        followed or by the user herself.
+        Tweets must be ordered from most recent to least recent.
         :type userId: int
         :rtype: List[int]
         """
