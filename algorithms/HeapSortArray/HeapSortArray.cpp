df9ae636b1c6dc44db5de822ae63be9464d6454b
wangyangtc <wangyangtc@gmail.com>
2018-06-06 23:09:18 -0700

    change

--- algorithms/HeapSortArray/HeapSortArray.cpp
+++ algorithms/HeapSortArray/HeapSortArray.cpp
@@ -1,40 +1,56 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-void heapify(int arr[], int n, int i)
-{
-   // Find largest among root, left child and right child
-   int largest = i;
-   int l = 2*i + 1;
-   int r = 2*i + 2;
+class Solution{
+public:
+    void heapify(vector<int>& num, int n, int i) {
+      int largest = i;
+      int l = 2 * i + 1;
+      int r = 2 * i + 2;
 
-   if (l < n && arr[l] > arr[largest])
-     largest = l;
+      // Find largest among root, left child and right child
+      if(l < n && num[l] > num[largest]) {
+        largest = l;
+      }
 
-   if (right < n && arr[r] > arr[largest])
-     largest = r;
+      if(r < n && num[r] > num[largest]) {
+        largest = r;
+      }
 
-   // Swap and continue heapifying if root is not largest
-   if (largest != i)
-   {
-     swap(arr[i], arr[largest]);
-     heapify(arr, n, largest);
-   }
-}
+      // Swap and continue heapifying if root is not largest
+      if(largest != i) {
+          swap(num[i], num[largest]);
+          heapify(num, n, largest);
+      }
+    }
 
-// main function to do heap sort
-void heapSort(int arr[], int n)
-{
-   // Build max heap
-   for (int i = n / 2 - 1; i >= 0; i--)
-     heapify(arr, n, i);
+    void heapSort(vector<int>& num){
+        int n = num.size();
 
-   // Heap sort
-   for (int i=n-1; i>=0; i--)
-   {
-     swap(arr[0], arr[i]);
-     
-     // Heapify root element to get highest element at root again
-     heapify(arr, i, 0);
-   }
+        // Build a heap
+        for(int i = n / 2 - 1; i >= 0; i--) {
+          heapify(num, n, i);
+        }
+
+        // Heap sort
+        for(int i = n - 1; i >= 0; i--) {
+          swap(num[0], num[i]);
+          heapify(num, i, 0);
+        }
+    }
+};
+
+int main(){
+    int array[] = {5, 8, 7, 1, 3, 2, 6, 4};
+    vector<int> num(array, array + sizeof(array) / sizeof(array[0]));
+
+    Solution solution;
+    solution.heapSort(num);
+
+    for(int i = 0; i < num.size(); i++)
+        cout << num[i] << " ";
+    cout << endl;
+    return 0;
 }
