82eefcb1efd66a5ddb077b109f0db681c996fba1
wangyangtc <wangyangtc@gmail.com>
2018-06-08 11:44:06 -0700

    add Rand5ToRand7

---
+++ algorithms/Rand5ToRand7/Rand5ToRand7.py
@@ -0,0 +1,16 @@
+# Given rand5 (1 ~ 5), implement rand7 (1 ~ 7)
+
+def rand7():
+  while True:
+      a = rand5();
+      b = rand5();
+
+      # num 1 ~ 25
+      num = (a - 1) * 5 + b
+  
+      # num 1 ~ 21    
+      if num < 22:
+          break
+  
+  return num % 7 + 1
+
