d8b8d4a35f7b9af09f5e5b515fdc59f6c3a2cb21
wangyangtc <wangyangtc@gmail.com>
2018-06-08 11:22:54 -0700

    minor

--- algorithms/MaximumSubarray/MaximumSubarray.cpp
+++ algorithms/MaximumSubarray/MaximumSubarray.cpp
@@ -5,30 +5,16 @@
 #include <vector>
 using namespace std;
 
-class Solution{
+class Solution {
 public:
-    int maxSubArray(int A[], int n){
-        int sum = 0;
-        int maxSum = INT_MIN;
-        for(int i = 0; i < n; i++){
-            sum += A[i];
-            if(sum > maxSum)
-                maxSum = sum;
-            if(sum < 0)
-                sum = 0;
-        }
-        return maxSum;
+  int maxSubArray(vector<int>& nums) {
+    int sum = 0;
+    int maxSum = INT_MIN;
+    for(int i = 0; i < nums.size(); i++) {
+      sum += nums[i];
+      maxSum = max(maxSum, sum);
+      sum = max(sum, 0);
     }
+    return maxSum;
+  }
 };
-
-int main(){
-    int A[] = {-1, 1};
-    int n = sizeof(A) / sizeof(A[0]);
-
-    Solution solution;
-    int res = solution.maxSubArray(A, n);
-    cout << res << endl;
-
-    return 0;
-}
-
