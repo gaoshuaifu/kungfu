9790e4b54834a898ea9786605696fbc8cf205a42
wangyangtc <wangyangtc@gmail.com>
2018-06-12 22:39:37 -0700

    add

--- algorithms/RemoveKDigits/RemoveKDigits.py
+++ algorithms/RemoveKDigits/RemoveKDigits.py
@@ -1,7 +1,30 @@
 class Solution(object):
     def removeKdigits(self, num, k):
+        # Make elements in stk are in increasing order.
+        stk = []
+        for c in num:
+            while k > 0 and stk and stk[-1] > c:
+                stk.pop()
+                k -= 1
+            stk.append(c)
+    
+        # Continue to remove if less than k elements have been removed.
+        while k:
+            stk.pop()
+            k -= 1
+        
+        # Remove leading 0s.
+        while stk and stk[0] == "0":
+            stk.pop(0)
+    
+        return "".join(stk) if stk else "0"  
+        
+ 
+class Solution(object):
+    def removeKdigits(self, num, k):
         res = ""
-        while k and k < len(num):
+        while k > 0 and k < len(num):
+            # In the first k + 1 elements, must keep at least 1, so keep the smallest.
             min_dgt = min(num[:k + 1])
             res += min_dgt
             min_idx = num.find(min_dgt)
