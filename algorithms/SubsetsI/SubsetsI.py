bfdbbd3e5355a15166e123eb3a09dff5e61092dc
wangyangtc <wangyangtc@gmail.com>
2018-06-13 08:44:15 -0700

    add

--- algorithms/SubsetsI/SubsetsI.py
+++ algorithms/SubsetsI/SubsetsI.py
@@ -6,3 +6,18 @@ class Solution(object):
             for i in range(s):
                 res.append(res[i] + [num])
         return res
+
+
+import copy
+class Solution(object):
+    def subsetsHelper(self, nums, start):
+        self.res.append(copy.deepcopy(self.sol))
+        for i in range(start, len(nums)):
+            self.sol.append(nums[i])
+            self.subsetsHelper(nums, i + 1)
+            self.sol.pop()
+
+    def subsets(self, nums):
+        self.sol, self.res = [], []
+        self.subsetsHelper(nums, 0)
+        return self.res
