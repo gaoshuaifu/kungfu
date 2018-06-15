9a80847335758a0e7736780de8369eef7d4268ed
wangyangtc <wangyangtc@gmail.com>
2018-06-15 11:43:42 -0700

    change

--- algorithms/ReversePairs/ReversePairs.py
+++ algorithms/ReversePairs/ReversePairs.py
@@ -1,3 +1,29 @@
+class Solution(object):
+    def _mergesort_and_count(self, nums, low, high):
+        if low >= high:
+            return 0
+
+        count, mid = 0, (low + high) / 2
+        count += self._mergesort_and_count(nums, low, mid)
+        count += self._mergesort_and_count(nums, mid + 1, high)
+
+        i, j = low, mid + 1
+        while i <= mid:
+            while j <= high and nums[i] > nums[j] * 2:
+                j += 1
+            count += j - (mid + 1)
+            i += 1
+
+        nums[low: high + 1] = sorted(nums[low: high + 1])
+        return count
+
+    def reversePairs(self, nums):
+        return self._mergesort_and_count(nums, 0, len(nums) - 1)
+
+
+
+
+
 class TreeNode(object):
     def __init__(self, val):
         self.val = val
@@ -5,7 +31,7 @@ class TreeNode(object):
         self.right = None
         self.cnt_ge = 1
 
-class Solution(object):
+class Solution1(object):
     def _insert(self, head, val):
         if not head:
             return TreeNode(val)
@@ -37,26 +63,5 @@ class Solution(object):
             head = self._insert(head, num)
         return count
 
-class Solution(object):
-    def _mergesort_and_count(self, nums, low, high):
-        if low >= high:
-            return 0
-
-        count = 0
-        mid = (low + high) / 2
-        count += self._mergesort_and_count(nums, low, mid)
-        count += self._mergesort_and_count(nums, mid + 1, high)
-
-        i = low
-        j = mid + 1
-        while i <= mid:
-            while j <= high and nums[i] > nums[j] * 2:
-                j += 1
-            count += j - (mid + 1)
-            i += 1
-
-        nums[low: high + 1] = sorted(nums[low: high + 1])
-        return count
-
-    def reversePairs(self, nums):
-        return self._mergesort_and_count(nums, 0, len(nums) - 1)
+s = Solution()
+print(s.reversePairs([2,4,3,5,1]))
